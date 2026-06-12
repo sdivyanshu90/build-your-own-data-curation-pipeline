63ae83e17efe108d74de60378bedaa04ddb0bac3
sdivyanshu90 <divyanshu74.80@gmail.com>
2026-06-12 16:16:12 +0530

    feat(clustering): add clustering package exports

---
+++ dedup_pipeline/clustering/__init__.py
@@ -0,0 +1,7 @@
+"""Clustering: Disjoint Set Union over candidate pairs."""
+
+from __future__ import annotations
+
+from dedup_pipeline.clustering.union_find import UnionFind
+
+__all__ = ["UnionFind"]
